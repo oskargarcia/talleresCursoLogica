2a232fada103ae9a3ec61197e1c7bc3b74bfe664
oskargarcia <100642468+oskargarcia@users.noreply.github.com>
2023-04-15 15:45:30 -0500

    taller 1 y taller 2
    archivos curso fundamentos de programacion

---
+++ taller1.py
@@ -0,0 +1,21 @@
+identificacion = 807255
+nombres = "oscar Raul "
+apellidos ="Garcia M"
+direccion = "calle 5 e # 12 - 20"
+telefono = 123456789
+estadoCivil = "eso depende"
+numeroHijos = 2
+estatura = 1.65
+fechaContratacion = "11/02/2023"
+sueldo = 1160000
+diasLaborados = 60
+
+print(f'Nombre completo: {nombres}{apellidos} \nNumero de identificacion: {identificacion}')
+print('-' *20)
+print(f'Direccion de recidencia: {direccion}\nNumero de contacto: {telefono}')
+print('-' *20)
+print(f'Estado civil: {estadoCivil}\nHijos: {numeroHijos}')
+print('-' *20)
+print(f"Estatura: {estatura}")
+print('-' *20)
+print(f'Fecha de contratacion: {fechaContratacion}\nSueldo basico: {sueldo}\nDias laborados: {diasLaborados}')
